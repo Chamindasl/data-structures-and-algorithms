8b0320c4bed9cb7274bbfd87c0ea2cbd6e5ed42a
Chaminda Amarasinghe <chaminda.sl@gmail.com>
2020-11-25 00:27:43 +0000

    Create 704. Binary Search.py

---
+++ python/704. Binary Search.py
@@ -0,0 +1,19 @@
+# 704. Binary Search
+# https://leetcode.com/problems/binary-search/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        mid = None
+        # left mid targt right
+        while left <= right : 
+            mid = left + (right - left) // 2
+            if (nums[mid] == target):
+                return mid                
+            if nums[mid] < target:
+                left = mid + 1
+            else: 
+                right = mid - 1
+        return -1
+            
+        
