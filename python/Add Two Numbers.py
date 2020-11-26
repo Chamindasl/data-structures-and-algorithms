0f349e90559dbefe7f37313fdc968c9b6f4f33ab
Chaminda Amarasinghe <chaminda.sl@gmail.com>
2020-11-26 04:28:40 +0000

    Create Add Two Numbers.py

---
+++ python/Add Two Numbers.py
@@ -0,0 +1,30 @@
+# 2. Add Two Numbers
+# https://leetcode.com/problems/add-two-numbers/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, min_list: ListNode, max_list: ListNode) ->ListNode:
+        cf = 0
+        dummy = ListNode(0)
+        head = dummy
+        while min_list is not None or max_list is not None:
+            min_val, max_val = 0,0
+            if min_list is not None:
+                min_val = min_list.val
+                min_list = min_list.next
+            if max_list is not None:
+                max_val = max_list.val
+                max_list = max_list.next
+            cf, tot = divmod(min_val + max_val + cf,10)
+            head.next = ListNode(tot)
+            head = head.next
+            
+        if cf !=0 :
+            head.next = ListNode(cf)
+        
+        return dummy.next
