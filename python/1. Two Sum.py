660144f116e64532d662b1ae918afedeea883cc3
Chaminda Amarasinghe <chaminda.sl@gmail.com>
2020-11-26 03:23:27 +0000

    Create 1. Two Sum.py

---
+++ python/1. Two Sum.py
@@ -0,0 +1,12 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+        for i, num in enumerate(nums):
+            if target - num in visited: 
+                return visited[target - num], i
+            visited[num] = i
+        return None
+        
