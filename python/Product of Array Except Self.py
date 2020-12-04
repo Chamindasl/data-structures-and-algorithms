9cf5af524ac64f51b820aac7b2595bfe6dec3b07
Chaminda Amarasinghe <chaminda.sl@gmail.com>
2020-12-04 18:44:35 +0000

    Update Product of Array Except Self.py

--- python/Product of Array Except Self.py
+++ python/Product of Array Except Self.py
@@ -3,12 +3,10 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_prod=  [1] * len(nums)
-        right_prod = [1] * len(nums)
-        for i in range(1,len(nums)):
-            left_prod[i] = left_prod[i-1] * nums[i-1]
-            right_prod[-i-1] = right_prod[-i] * nums[-i]
-        for i in range(len(nums)):
-            nums[i] = left_prod[i] * right_prod[i]
+        to_left_prod, to_right_prod = [1] * len(nums), [1] * len(nums)
         
-        return nums
+        for i in range (1, len(nums)):
+            to_left_prod[i] = to_left_prod[i-1] * nums[i-1] 
+            to_right_prod[-i - 1] = to_right_prod[-i - 1 +1] * nums[-i - 1 +1]
+        
+        return [to_left_prod[i] * to_right_prod[i] for i in range(len(nums))]
